c76fb0736a0e36bd7cbd08645704347f17a7b1ac
Gr00ver90 <egor822568@gmail.com>
2021-11-23 16:01:56 +0900

    Initial commit

---
+++ app/src/main/java/com/itcube/dz5/Clock.java
@@ -0,0 +1,16 @@
+package com.itcube.dz5;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
---
+++ app/src/main/java/com/itcube/dz5/Illusion.java
@@ -0,0 +1,16 @@
+package com.itcube.dz5;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Illusion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_illusion);
+    }
+}
